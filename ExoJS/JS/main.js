f99c404d5f774c6f6c724855b06317a12f5d38b1
VanessaBlanchet <miminemail@gmail.com>
2023-07-21 12:16:54 +0200

    WIP : la boucle while

--- ExoJS/JS/main.js
+++ ExoJS/JS/main.js
@@ -51,23 +51,26 @@
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
 
-const motsApplication = ["Cachalot", "Pétunia", "Serviette"]
-let score = 0
+//! Condition avec if
 
-let motUtilisateur = prompt ("Entrez le mot : " + motsApplication[0])
+// const motsApplication = ["Cachalot", "Pétunia", "Serviette"]
+// let score = 0
 
-// Je vérifie que je récupère bien le mot entré par l'utilisateur : 
-// console.log(motUtilisateur);
+// let motUtilisateur = prompt ("Entrez le mot : " + motsApplication[0])
 
-if (motUtilisateur === motsApplication[0]){
-    console.log("Bravo !")
-    score += 1
-    console.log("Score : " + score)
-} else {
-    console.log("Faux ! ")
-    console.log("Score : " + score);
-}
+// // Je vérifie que je récupère bien le mot entré par l'utilisateur : 
+// // console.log(motUtilisateur);
 
+// if (motUtilisateur === motsApplication[0]){
+//     console.log("Bravo !")
+//     score += 1
+//     console.log("Score : " + score)
+// } else {
+//     console.log("Faux ! ")
+//     console.log("Score : " + score);
+// }
+
+//! condition avec Switch
 
 // switch (motUtilisateur){
 //     case motsApplication[0]:
@@ -82,17 +85,20 @@ if (motUtilisateur === motsApplication[0]){
     
 // }
 
-motUtilisateur = prompt ("Entrez le mot : " + motsApplication[1])
+//! Condition avec if
 
-if (motUtilisateur === motsApplication[1]){
-    console.log("Bravo !")
-    score += 1
-    console.log("Score : " + score);
-}  else {
-    console.log("Faux ! ")
-    console.log("Score : " + score);
-}
+// motUtilisateur = prompt ("Entrez le mot : " + motsApplication[1])
 
+// if (motUtilisateur === motsApplication[1]){
+//     console.log("Bravo !")
+//     score += 1
+//     console.log("Score : " + score);
+// }  else {
+//     console.log("Faux ! ")
+//     console.log("Score : " + score);
+// }
+
+//! Condition avec Switch
 
 // motUtilisateur = prompt ("Entrez le mot : " + motsApplication[1])
 
@@ -108,17 +114,20 @@ if (motUtilisateur === motsApplication[1]){
 //             console.log("Score :" + score);
     
 // }
+//! Condition avec if
 
-motUtilisateur = prompt ("Entrez le mot : " + motsApplication[2])
+// motUtilisateur = prompt ("Entrez le mot : " + motsApplication[2])
 
-if (motUtilisateur === motsApplication[2]){
-    console.log("Bravo !")
-    score += 1
-    console.log("Score : " + score);
-}  else {
-    console.log("Faux ! ")
-    console.log("Score : " + score);
-}
+// if (motUtilisateur === motsApplication[2]){
+//     console.log("Bravo !")
+//     score += 1
+//     console.log("Score : " + score);
+// }  else {
+//     console.log("Faux ! ")
+//     console.log("Score : " + score);
+// }
+
+//! Condition avec Switch
 
 // motUtilisateur = prompt ("Entrez le mot : " + motsApplication[2])
 
@@ -142,4 +151,47 @@ if (motUtilisateur === motsApplication[2]){
 // ------------------------------------------------------------------
 // ------------------------------------------------------------------
 
+const motsApplication = ["Cachalot", "Pétunia", "Serviette"]
+i = 0
+score = 0
 
+//Je vérifie que je récupère bien le mot : ça marche
+
+// console.log("motApplication : " + motsApplication[i])
+console.log("Id initial : " + i);
+
+// Je vérifie que je récupère bien} le mot entré par l'utilisateur : ça marche 
+
+motUtilisateur = prompt ("Entrez le mot : " + motsApplication[i])
+
+// console.log("motUtilisateur : " + motUtilisateur)
+
+
+
+while (i < 3){
+
+    console.log("Id du mot demandé : " + i)
+    console.log("Mot attendu : " + motsApplication[i])
+    console.log("Mot tapé : " + motUtilisateur)
+    
+    if (motUtilisateur === motsApplication[i]){
+    console.log("Bravo !")
+    score ++
+    console.log("Score : " + score)
+    i++
+    motUtilisateur = prompt ("Entrez le mot : " + motsApplication[i])
+
+    } else {
+        console.log("Faux !")
+        console.log("Score : " + score)
+        i++
+        motUtilisateur = prompt ("Entrez le mot : " + motsApplication[i])
+        
+    }
+
+    console.log(motUtilisateur);
+
+}
+
+
+// Il y a un problème dans le code ci-dessus : le prompt apparaît une fois de trop en me demandant un mot de plus (undefined, du coup, c'est logique puisqu'il n'y a que 3 mots dans la liste des mots de l'application)
