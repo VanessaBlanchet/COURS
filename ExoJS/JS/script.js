8f28f1b287a649a80668ede851e43f229028f867
VanessaBlanchet <miminemail@gmail.com>
2023-07-27 15:45:48 +0200

    je récupère un élément html dans JS : ok

--- ExoJS/JS/script.js
+++ ExoJS/JS/script.js
@@ -20,3 +20,16 @@ console.log("Liste inputRadio : " + listeInputRadio)
 
 //! WIP à revoir, ça n'a pas l'air de produire le résultat escompté
 // https://openclassrooms.com/fr/courses/7696886-apprenez-a-programmer-avec-javascript/8205925-recuperez-un-element-d-une-page-web
+
+
+//! Récupérer des éléments de la page HTML et les modifier
+
+// J'ai ajouté un id à mon Span dans la page index.html afin de pouvoir affiché en front
+// le mot demandé. Je le récupère ici, puis je vérifie que ça marche en faisant un console.log
+
+let motAffiche = document.getElementById("motAffiche")
+
+console.log("Mot Affiché : " + motAffiche);
+
+// ça marche
+
